19d9588fd5e9c8d5d006143a7d0b61f6e6ee0ef2
Christian Ramon <rchristian.ramon@gmail.com>
2014-05-11 12:29:25 -0500

    adding states array to scope.

--- extending-angular-ui-typeahead/app/scripts/controllers/main.js
+++ extending-angular-ui-typeahead/app/scripts/controllers/main.js
@@ -1,10 +1,6 @@
 'use strict';
 
 angular.module('extendingAngularUiTypeaheadApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('MainCtrl', function ($scope, StateService) {
+    $scope.states = StateService.getAll();
   });
